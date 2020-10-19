ebc8d328ba1e25b8866bb1bcd1dd820dd593e895
wangjiwei02 <wangjiwei02@58.com>
2020-10-19 20:11:12 +0800

    http tool

---
+++ avalon/common/src/main/java/com/avalon/ms/common/http/HttpClientConnection.java
@@ -0,0 +1,85 @@
+package com.avalon.ms.common.http;
+
+import com.caucho.hessian.client.HessianConnection;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.message.BasicHeader;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+
+/**
+ * @Description:
+ * @Author: saberey
+ * @Date: 2020/10/19 19:42
+ * @Version: 1.0
+ */
+public class HttpClientConnection  implements HessianConnection {
+
+    private final HttpClient httpClient;
+
+    private final ByteArrayOutputStream output;
+
+    private final HttpPost request;
+
+    private volatile HttpResponse response;
+
+    public HttpClientConnection(HttpClient httpClient, URL url) {
+        this.httpClient = httpClient;
+        this.output = new ByteArrayOutputStream();
+        this.request = new HttpPost(url.toString());
+    }
+
+    @Override
+    public void addHeader(String key, String value) {
+        request.addHeader(new BasicHeader(key, value));
+    }
+
+    @Override
+    public OutputStream getOutputStream() throws IOException {
+        return output;
+    }
+
+    @Override
+    public void sendRequest() throws IOException {
+        request.setEntity(new ByteArrayEntity(output.toByteArray()));
+        this.response = httpClient.execute(request);
+    }
+
+    @Override
+    public int getStatusCode() {
+        return response == null || response.getStatusLine() == null ? 0 : response.getStatusLine().getStatusCode();
+    }
+
+    @Override
+    public String getStatusMessage() {
+        return response == null || response.getStatusLine() == null ? null : response.getStatusLine().getReasonPhrase();
+    }
+
+    @Override
+    public String getContentEncoding() {
+        return (response == null || response.getEntity() == null || response.getEntity().getContentEncoding() == null) ? null : response.getEntity().getContentEncoding().getValue();
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        return response == null || response.getEntity() == null ? null : response.getEntity().getContent();
+    }
+
+    @Override
+    public void close() throws IOException {
+        HttpPost request = this.request;
+        if (request != null) {
+            request.abort();
+        }
+    }
+
+    @Override
+    public void destroy() throws IOException {
+    }
+}
---
+++ avalon/common/src/main/java/com/avalon/ms/common/http/HttpClientConnectionFactory.java
@@ -0,0 +1,11 @@
+package com.avalon.ms.common.http;
+
+/**
+ * @Description:
+ * @Author: wangjiwei02
+ * @Date: 2020/10/19 19:42
+ * @Version: 1.0
+ */
+public class HttpClientConnectionFactory {
+
+}
